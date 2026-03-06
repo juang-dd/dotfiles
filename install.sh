e5b10cc8fadf6ba0109bbe5f9cd1ecb7b127d765
Juan Garcia <juant.garcia@datadoghq.com>
2026-03-06 14:47:09 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,3 +12,8 @@ git clone git@github.com:DataDog/experimental.git ~/dd/experimental
 cd ~/dd/experimental/users/juant.garcia/dotfiles
 # symlinks the files in dotfiles/ to the workspace, gives priority to dotfiles.
 make force-install
+
+# Symlink experimental skills
+for skill in ~/dd/experimental/teams/aaa-user/skills/*/; do
+  ln -sf "$skill" ~/.claude/skills/$(basename "$skill")
+done
