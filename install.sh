dbdf1c24201c54bcd5b5c15e29d90500f8beeca3
Juan Garcia <juant.garcia@datadoghq.com>
2026-03-06 09:32:28 -0500

    Update install script

--- install.sh
+++ install.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
-# ensure you set the executable bit on the file with `chmod u+x install.sh`
-
-# If you remove the .example extension from the file, once your workspace is created and the contents of this
-# repo are copied into it, this script will execute.  This will happen in place of the default behavior of the workspace system,
-# which is to symlink the dotfiles copied from this repo to the home directory in the workspace.
-
 
 set -euo pipefail
 
@@ -16,9 +10,5 @@ git clone git@github.com:DataDog/experimental.git ~/dd/experimental
 
 # Install all packages (on a fresh machine)
 cd ~/dd/experimental/users/juant.garcia/dotfiles
-make install
-
-# If configs already exist, adopt them first
-make adopt        # moves existing files into dotfiles/, creates symlinks
-git diff          # review what differs from managed versions
-git checkout .    # keep the managed versions (or commit to keep yours)
+# symlinks the files in dotfiles/ to the workspace, gives priority to dotfiles.
+make force-install
